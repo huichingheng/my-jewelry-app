ae150c25b14bdeb7359a4bd01afe2f323a3d6bf0
huichingheng <huichingheng@gmail.com>
2018-09-15 02:22:42 +0800

    change classname

--- src/component/Logo.js
+++ src/component/Logo.js
@@ -4,7 +4,7 @@ import "../style/Logo.css";
 
 const Logo = () => {
   return (
-    <div className="container">
+    <div className="logo-container">
       <img src={logo} className="logo" alt="logo" />
     </div>
   );
